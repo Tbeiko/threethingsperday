ccfc787d7d4f14908ae759ffd130d8fe09515204
Tim Beiko <t.beiko23@gmail.com>
2015-10-10 11:12:14 -0400

    build initial few pages

---
+++ db/migrate/20151010145021_todos.rb
@@ -0,0 +1,10 @@
+class Todos < ActiveRecord::Migration
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :description
+      t.string :status
+      t.timestamps
+    end
+  end
+end
