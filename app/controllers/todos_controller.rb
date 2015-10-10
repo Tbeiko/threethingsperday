fbd6b252a882012fc4bf4a8639a264f2ac6d46c0
Tim Beiko <t.beiko23@gmail.com>
2015-10-10 14:43:51 -0400

    few changes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,18 +1,10 @@
 class TodosController < ApplicationController
-
-
-
-
   def index
   end
-
-
 
   def new
   	@post = Todo.new
   end
-
-
 
   def create
   	@post = Todo.new(post_params)
@@ -27,15 +19,9 @@ class TodosController < ApplicationController
   	@posts = current_user.posts.all
   end
 
-  
-
-
   private
   def post_params
   	params.require(:post).permit(:title, :description, :status) #description = failure description
   end
-
-
-
 
 end
