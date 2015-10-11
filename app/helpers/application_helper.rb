ffec199bc72877d4cdc18c821430dc37b86936b1
Tim Beiko <t.beiko23@gmail.com>
2015-10-11 12:02:52 -0400

    style changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def card_color(todo)
+    if todo.status == nil
+      "bg-info"
+    elsif todo.status == "completed"
+      "bg-success"
+    elsif todo.status == "failed"
+      "bg-danger"
+    end
+  end
 end
